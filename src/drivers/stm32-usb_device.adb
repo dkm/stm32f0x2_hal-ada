63bd222fedb1491ce232b4023b9be9c64132213f
Marc Poulhiès <dkm@kataplop.net>
2022-07-04 00:02:46 +0200

    WIP

--- src/drivers/stm32-usb_device.adb
+++ src/drivers/stm32-usb_device.adb
@@ -4,12 +4,12 @@
 --                                                                           --
 --                  Copyright (C) 2022      Marc Poulhiès                    --
 --                                                                           --
---    Wee Noise Maker is free software: you can redistribute it and/or       --
+--    STM32F0 USB is free software: you can redistribute it and/or           --
 --    modify it under the terms of the GNU General Public License as         --
 --    published by the Free Software Foundation, either version 3 of the     --
 --    License, or (at your option) any later version.                        --
 --                                                                           --
---    Wee Noise Maker is distributed in the hope that it will be useful,     --
+--    STM32F0 USB is distributed in the hope that it will be useful,         --
 --    but WITHOUT ANY WARRANTY; without even the implied warranty of         --
 --    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       --
 --    General Public License for more details.                               --
